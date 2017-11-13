1913fc7d8ff868b78d0f48fd87c260b4e068bcb0
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-13 16:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newKittens = kittens.splice(kittens.length-1,1);
+  var newKittens = kittens.slice(kittens.length-1);
   return newKittens;
 }
